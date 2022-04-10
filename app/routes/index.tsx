1ba3c8cbf87c2a9da8ed080b0f498da5e05280ab
Yashwant <yashwant8530@gmail.com>
2022-04-10 22:23:46 +0530

    Initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <main className="">
+      <h1>Welcome to Remix</h1>
+    </main>
+  )
+}
---
+++ remix.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+module.exports = {
+  cacheDirectory: './node_modules/.cache/remix',
+  ignoredRouteFiles: ['.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+}
